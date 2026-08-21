5cecf29f305521dfa53b863c4665b2abc762575e
BANGSULSTART <99018264+BangsulBotz@users.noreply.github.com>
2026-08-21 22:48:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,60 @@ import { connectionHandler } from './src/connectionHandler.js'
 import { messageHandler } from './src/messageHandler.js'
 import { groupEventHandler } from './src/groupEventHandler.js'
 
-const sock = createSocket()
-
-connectionHandler(sock)
-groupEventHandler(sock)
-await messageHandler(sock)
-
-process.on('SIGINT', async () => {
-  console.log(chalk.yellow('\n[WA] Menutup koneksi...'))
-  await sock.disconnect()
-  process.exit(0)
-})
+let sock
 
 try {
+  sock = createSocket()
+
+  connectionHandler(sock)
+  groupEventHandler(sock)
+
+  const messageHandlerResult = messageHandler(sock)
+
+  if (messageHandlerResult instanceof Promise) {
+    messageHandlerResult.catch(err => {
+      console.error(
+        chalk.red('[MESSAGE] Handler error:'),
+        err?.stack || err?.message || err
+      )
+    })
+  }
+
   await sock.connect()
+
 } catch (err) {
-  console.error(chalk.red('[WA] Gagal connect awal:'), err?.message || err)
+  console.error(
+    chalk.red('[WA] Gagal connect awal:'),
+    err?.stack || err?.message || err
+  )
+
   process.exit(1)
 }
+
+let shuttingDown = false
+
+async function shutdown(signal) {
+  if (shuttingDown) return
+
+  shuttingDown = true
+
+  console.log(
+    chalk.yellow(`\n[WA] Menerima ${signal}, menutup koneksi...`)
+  )
+
+  try {
+    if (sock) {
+      await sock.disconnect()
+    }
+  } catch (err) {
+    console.error(
+      chalk.red('[WA] Gagal menutup koneksi:'),
+      err?.stack || err?.message || err
+    )
+  }
+
+  process.exit(0)
+}
+
+process.on('SIGINT', () => shutdown('SIGINT'))
+process.on('SIGTERM', () => shutdown('SIGTERM'))
