19cca0fc9a8ce93787bb16c4fa14ee635732b058
BANGSULSTART <99018264+BangsulBotz@users.noreply.github.com>
2026-08-25 01:15:36 +0800

    ping: diringkas jadi satu ekspresi

--- plugins/bot/ping.js
+++ plugins/bot/ping.js
@@ -1,12 +1,8 @@
 // plugins/bot/ping.js
-
 export default {
     command: 'ping',
     alias: ['test', 'p2', 'mbut'],
     category: 'bot',
     description: 'Cek latency bot.',
-
-    async execute(m) {
-        await m.reply('🏓 Pong!')
-    }
+    execute: m => m.reply('🏓 Pong!')
 }
